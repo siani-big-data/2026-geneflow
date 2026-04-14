32a9bf00b270d0abe83c16a4deb4b1ac88af1cf8
edumarrero <edumarreroglezz@gmail.com>
2026-04-15 00:45:56 +0200

    feat(utils): add file operation utilities

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,12 +1,18 @@
 """Utility modules for GeneFlow Datalake."""
 
 from src.utils.dates import format_date, format_datetime, parse_date, parse_datetime
+from src.utils.files import append_jsonl_file, read_jsonl_file, read_lines, safe_unlink, write_lines
 from src.utils.logging import log_execution
 
 __all__ = [
+    "append_jsonl_file",
     "format_date",
     "format_datetime",
     "log_execution",
     "parse_date",
     "parse_datetime",
+    "read_jsonl_file",
+    "read_lines",
+    "safe_unlink",
+    "write_lines",
 ]
